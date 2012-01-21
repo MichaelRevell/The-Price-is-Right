8acc59c36b3db3f886216bcca469c40c4195c823
Michael <mars8082686@gmail.com>
2012-01-21 18:58:09 -0500

    added score

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,10 +11,34 @@ class PagesController < ApplicationController
     
     if price.to_i == @item.price.to_i
       @text = "You win"
+      add_to_score
     else
       @text = "You lose.  price: #{@item.price}"
+      subtract_from_score
     end
     
   end
-
+  
+  
+  # THE FOLLOWING SHOULD NOT BE HERE
+  # PLEASE REFACTOR THIS AT SOME POINT
+  # PLEASE, PLEASE, PLEASE
+  # WHY IS THIS HERE MICHAEL????
+  private 
+  
+  def add_to_score
+    session[:score] += 10
+  end
+  
+  def substract_from_score
+    if session[:score]-10 > 0
+      session[:score] -= 10 
+    elsif session[:score] > 0
+      session[:score] = 0
+    else 
+      session[:score] = 0
+    end
+  end
+  # EVERYTHING ABOVE SHOULD BE PUT SOMEWHERE ELSE
+  # IE IT SHOULD BE HERE!!
 end
