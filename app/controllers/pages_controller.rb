b9320ed6abc393334d89e53725f5ff1d6131618a
Michael <mars8082686@gmail.com>
2012-01-21 18:35:41 -0500

    added result

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,18 @@ class PagesController < ApplicationController
     @item = Item.first if Item.first
     @item.create_choices
   end
+  
+  def result
+    price = params[:q]
+    @item = Item.first if Item.first
+    @item.create_choices
+    
+    if price.to_i == @item.price.to_i
+      @text = "You win"
+    else
+      @text = "You lose.  price: #{@item.price}"
+    end
+    
+  end
 
 end
