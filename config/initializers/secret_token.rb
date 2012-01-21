633168487d2c3f93b96d86b21fe6cfc94116bbd1
Michael <mars8082686@gmail.com>
2012-01-21 14:58:44 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThePriceIsRight::Application.config.secret_token = 'f7fd751fe3be8e68e4c78787d9c78666af473df78caba8bec4ddee8702e4f4c4aedabeee587666c802bb2ace6760379bdf8ded9db393e32873bef2be33376eca'
