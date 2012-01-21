3b15b1e31e3b4e8c3dbf3795cc1ab3b6a512789f
Michael <mars8082686@gmail.com>
2012-01-21 15:23:58 -0500

    added pages and items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ThePriceIsRight::Application.routes.draw do
+  resources :items
+
+  get "pages/home"
+  
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
