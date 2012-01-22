4394c90f2afd00518b701293ccd8869b559a5119
Michael <mars8082686@gmail.com>
2012-01-22 12:30:31 -0500

    score

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def score
     session[:score] = 0 if session[:score].nil?
+    session[:score] = 0 if params[:score].nil? == false
     return session[:score]
   end 
   
